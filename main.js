5316965643ebe5fdaddbf6856e206e13b626d2f6
Taehun Lee <91559425+Hentyphoon@users.noreply.github.com>
2024-09-11 12:35:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.js';;
 
 const scene = new THREE.Scene(); // Base scene
 const view = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); // Camera
